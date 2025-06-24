8ddd6d862a8b752c0fa265663ce2cc9a3846fe16
UseTheForce007 <buttyusuf@gmail.com>
2025-06-24 12:42:07 +0100

    Refactor hand detection logic to streamline gesture processing and improve frame display

--- RealTimeCVHalide/basic.py
+++ RealTimeCVHalide/basic.py
@@ -24,15 +24,34 @@ while True:
     # Detect hands and get handedness, landmarks, and gestures
     hand_detected_frame, hand_info = handDetection.detect_hands(frame)
 
-    # Display detected hand information
-    for hand in hand_info:
-        print(f"Hand: {hand['handedness']}")
-        print(f"Gesture: {hand['gesture']}")
-        # print(f"Landmarks: {hand['landmarks']}")
+    if not hand_info:
+        continue
+    elif hand_info:
+        print(f"Shape of frame: {frame.shape}")
+        # Apply filters based on detected hand gestures
+        for hand in hand_info:
+            if hand["handedness"] == "Left":  # Process only the left hand
+                gesture = hand["gesture"]
+                print(f"Detected Gesture: {gesture} for Left Hand")
+
+                if gesture == "First Finger":
+                    frame = greyscale.greyscale_bgr_cv(frame)
+                elif gesture == "Two Fingers":
+                    frame = blur.blur_bgr_cv(frame)
+                elif gesture == "Open Hand":
+                    frame = pixelate.pixelate_cv2(frame)
+
+        cv2.imshow("Feed", frame)
+
+    # # Display detected hand information
+    # for hand in hand_info:
+    #     print(f"Hand: {hand['handedness']}")
+    #     print(f"Gesture: {hand['gesture']}")
+    #     # print(f"Landmarks: {hand['landmarks']}")
 
     # Display the original frame and the hand detection frame
-    cv2.imshow("Original Webcam Feed", frame)
-    cv2.imshow("Hand Detection Webcam Feed", hand_detected_frame)
+    # cv2.imshow("Original Webcam Feed", frame)
+    # cv2.imshow("Hand Detection Webcam Feed", hand_detected_frame)
 
     # Exit on 'q' key
     if cv2.waitKey(1) & 0xFF == ord("q"):
